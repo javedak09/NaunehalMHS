2de81d91ae6c9b6a715a7c4f6d27a654fe46dee0
javed.khan <javedak09@gmail.com>
2020-11-10 14:55:17 +0500

    Add project files.

---
+++ Win_Form_GB/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Win_Form_GB
+{
+    partial class InfoActivity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_s1qno = new System.Windows.Forms.Label();
+            this.label_s1q4 = new System.Windows.Forms.Label();
+            this.label_s1q6 = new System.Windows.Forms.Label();
+            this.label_s1q8 = new System.Windows.Forms.Label();
+            this.label_s1q11 = new System.Windows.Forms.Label();
+            this.label_s1q12 = new System.Windows.Forms.Label();
+            this.label_s1q13 = new System.Windows.Forms.Label();
+            this.label_s1q14 = new System.Windows.Forms.Label();
+            this.label_s1q15 = new System.Windows.Forms.Label();
+            this.label_s1q16 = new System.Windows.Forms.Label();
+            this.text_s1qno = new System.Windows.Forms.TextBox();
+            this.date_s1q4 = new System.Windows.Forms.DateTimePicker();
+            this.label_s1q2 = new System.Windows.Forms.Label();
+            this.text_s1q2 = new System.Windows.Forms.TextBox();
+            this.combo_s1q6 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.combo_s1q8 = new System.Windows.Forms.ComboBox();
+            this.text_s1q11 = new System.Windows.Forms.TextBox();
+            this.text_s1q12 = new System.Windows.Forms.TextBox();
+            this.text__s1q14 = new System.Windows.Forms.TextBox();
+            this.text__s1q15 = new System.Windows.Forms.TextBox();
+            this.text_s1q16 = new System.Windows.Forms.TextBox();
+            this.radio_s1q1301 = new System.Windows.Forms.RadioButton();
+            this.radio_s1q1302 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 911);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label_s1qno
+            // 
+            this.label_s1qno.AutoSize = true;
+            this.label_s1qno.Location = new System.Drawing.Point(62, 26);
+            this.label_s1qno.Name = "label_s1qno";
+            this.label_s1qno.Size = new System.Drawing.Size(115, 13);
+            this.label_s1qno.TabIndex = 1;
+            this.label_s1qno.Text = "Questionnaier Number:";
+            this.label_s1qno.Click += new System.EventHandler(this.s1qno_Click);
+            // 
+            // label_s1q4
+            // 
+            this.label_s1q4.AutoSize = true;
+            this.label_s1q4.Location = new System.Drawing.Point(62, 57);
+            this.label_s1q4.Name = "label_s1q4";
+            this.label_s1q4.Size = new System.Drawing.Size(88, 13);
+            this.label_s1q4.TabIndex = 2;
+            this.label_s1q4.Text = "Date of Interview";
+            this.label_s1q4.Click += new System.EventHandler(this.s1q4_Click);
+            // 
+            // label_s1q6
+            // 
+            this.label_s1q6.AutoSize = true;
+            this.label_s1q6.Location = new System.Drawing.Point(62, 124);
+            this.label_s1q6.Name = "label_s1q6";
+            this.label_s1q6.Size = new System.Drawing.Size(81, 13);
+            this.label_s1q6.TabIndex = 3;
+            this.label_s1q6.Text = "Province name:";
+            this.label_s1q6.Click += new System.EventHandler(this.s1q6_Click);
+            // 
+            // label_s1q8
+            // 
+            this.label_s1q8.AutoSize = true;
+            this.label_s1q8.Location = new System.Drawing.Point(62, 157);
+            this.label_s1q8.Name = "label_s1q8";
+            this.label_s1q8.Size = new System.Drawing.Size(71, 13);
+            this.label_s1q8.TabIndex = 4;
+            this.label_s1q8.Text = "District name:";
+            this.label_s1q8.Click += new System.EventHandler(this.s1q8_Click);
+            // 
+            // label_s1q11
+            // 
+            this.label_s1q11.AutoSize = true;
+            this.label_s1q11.Location = new System.Drawing.Point(62, 187);
+            this.label_s1q11.Name = "label_s1q11";
+            this.label_s1q11.Size = new System.Drawing.Size(96, 13);
+            this.label_s1q11.TabIndex = 5;
+            this.label_s1q11.Text = "Household number";
+            this.label_s1q11.Click += new System.EventHandler(this.s1q11_Click);
+            // 
+            // label_s1q12
+            // 
+            this.label_s1q12.AutoSize = true;
+            this.label_s1q12.Location = new System.Drawing.Point(62, 224);
+            this.label_s1q12.Name = "label_s1q12";
+            this.label_s1q12.Size = new System.Drawing.Size(101, 13);
+            this.label_s1q12.TabIndex = 6;
+            this.label_s1q12.Text = "Name of Household";
+            this.label_s1q12.Click += new System.EventHandler(this.s1q12_Click);
+            // 
+            // label_s1q13
+            // 
+            this.label_s1q13.AutoSize = true;
+            this.label_s1q13.Location = new System.Drawing.Point(62, 260);
+            this.label_s1q13.Name = "label_s1q13";
+            this.label_s1q13.Size = new System.Drawing.Size(137, 13);
+            this.label_s1q13.TabIndex = 1;
+            this.label_s1q13.Text = "Sex of the household head:";
+            this.label_s1q13.Click += new System.EventHandler(this.s1qno_Click);
+            // 
+            // label_s1q14
+            // 
+            this.label_s1q14.AutoSize = true;
+            this.label_s1q14.Location = new System.Drawing.Point(62, 291);
+            this.label_s1q14.Name = "label_s1q14";
+            this.label_s1q14.Size = new System.Drawing.Size(105, 13);
+            this.label_s1q14.TabIndex = 2;
+            this.label_s1q14.Text = "Respondent´s name:";
+            this.label_s1q14.Click += new System.EventHandler(this.s1q4_Click);
+            // 
+            // label_s1q15
+            // 
+            this.label_s1q15.AutoSize = true;
+            this.label_s1q15.Location = new System.Drawing.Point(62, 324);
+            this.label_s1q15.Name = "label_s1q15";
+            this.label_s1q15.Size = new System.Drawing.Size(102, 13);
+            this.label_s1q15.TabIndex = 3;
+            this.label_s1q15.Text = "Age of Respondent:";
+            this.label_s1q15.Click += new System.EventHandler(this.s1q6_Click);
+            // 
+            // label_s1q16
+            // 
+            this.label_s1q16.AutoSize = true;
+            this.label_s1q16.Location = new System.Drawing.Point(62, 357);
+            this.label_s1q16.Name = "label_s1q16";
+            this.label_s1q16.Size = new System.Drawing.Size(162, 13);
+            this.label_s1q16.TabIndex = 4;
+            this.label_s1q16.Text = "Years of schooling of respondent";
+            this.label_s1q16.Click += new System.EventHandler(this.s1q8_Click);
+            // 
+            // text_s1qno
+            // 
+            this.text_s1qno.Location = new System.Drawing.Point(240, 23);
+            this.text_s1qno.Name = "text_s1qno";
+            this.text_s1qno.Size = new System.Drawing.Size(100, 20);
+            this.text_s1qno.TabIndex = 7;
+            // 
+            // date_s1q4
+            // 
+            this.date_s1q4.Location = new System.Drawing.Point(240, 51);
+            this.date_s1q4.Name = "date_s1q4";
+            this.date_s1q4.Size = new System.Drawing.Size(200, 20);
+            this.date_s1q4.TabIndex = 8;
+            // 
+            // label_s1q2
+            // 
+            this.label_s1q2.AutoSize = true;
+            this.label_s1q2.Location = new System.Drawing.Point(62, 92);
+            this.label_s1q2.Name = "label_s1q2";
+            this.label_s1q2.Size = new System.Drawing.Size(99, 13);
+            this.label_s1q2.TabIndex = 2;
+            this.label_s1q2.Text = "Interviewer´s name:";
+            this.label_s1q2.Click += new System.EventHandler(this.s1q4_Click);
+            // 
+            // text_s1q2
+            // 
+            this.text_s1q2.Location = new System.Drawing.Point(240, 89);
+            this.text_s1q2.Name = "text_s1q2";
+            this.text_s1q2.Size = new System.Drawing.Size(100, 20);
+            this.text_s1q2.TabIndex = 7;
+            // 
+            // combo_s1q6
+            // 
+            this.combo_s1q6.FormattingEnabled = true;
+            this.combo_s1q6.Items.AddRange(new object[] {
+            "abc",
+            "def"});
+            this.combo_s1q6.Location = new System.Drawing.Point(240, 121);
+            this.combo_s1q6.Name = "combo_s1q6";
+            this.combo_s1q6.Size = new System.Drawing.Size(121, 21);
+            this.combo_s1q6.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Household number";
+            this.label2.Click += new System.EventHandler(this.s1q11_Click);
+            // 
+            // combo_s1q8
+            // 
+            this.combo_s1q8.FormattingEnabled = true;
+            this.combo_s1q8.Items.AddRange(new object[] {
+            "abc",
+            "def"});
+            this.combo_s1q8.Location = new System.Drawing.Point(240, 154);
+            this.combo_s1q8.Name = "combo_s1q8";
+            this.combo_s1q8.Size = new System.Drawing.Size(121, 21);
+            this.combo_s1q8.TabIndex = 9;
+            // 
+            // text_s1q11
+            // 
+            this.text_s1q11.Location = new System.Drawing.Point(240, 184);
+            this.text_s1q11.Name = "text_s1q11";
+            this.text_s1q11.Size = new System.Drawing.Size(100, 20);
+            this.text_s1q11.TabIndex = 7;
+            // 
+            // text_s1q12
+            // 
+            this.text_s1q12.Location = new System.Drawing.Point(240, 221);
+            this.text_s1q12.Name = "text_s1q12";
+            this.text_s1q12.Size = new System.Drawing.Size(100, 20);
+            this.text_s1q12.TabIndex = 7;
+            // 
+            // text__s1q14
+            // 
+            this.text__s1q14.Location = new System.Drawing.Point(240, 288);
+            this.text__s1q14.Name = "text__s1q14";
+            this.text__s1q14.Size = new System.Drawing.Size(100, 20);
+            this.text__s1q14.TabIndex = 7;
+            // 
+            // text__s1q15
+            // 
+            this.text__s1q15.Location = new System.Drawing.Point(240, 321);
+            this.text__s1q15.Name = "text__s1q15";
+            this.text__s1q15.Size = new System.Drawing.Size(100, 20);
+            this.text__s1q15.TabIndex = 7;
+            // 
+            // text_s1q16
+            // 
+            this.text_s1q16.Location = new System.Drawing.Point(240, 354);
+            this.text_s1q16.Name = "text_s1q16";
+            this.text_s1q16.Size = new System.Drawing.Size(100, 20);
+            this.text_s1q16.TabIndex = 7;
+            this.text_s1q16.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // radio_s1q1301
+            // 
+            this.radio_s1q1301.AutoSize = true;
+            this.radio_s1q1301.Location = new System.Drawing.Point(240, 256);
+            this.radio_s1q1301.Name = "radio_s1q1301";
+            this.radio_s1q1301.Size = new System.Drawing.Size(85, 17);
+            this.radio_s1q1301.TabIndex = 11;
+            this.radio_s1q1301.TabStop = true;
+            this.radio_s1q1301.Text = "radioButton2";
+            this.radio_s1q1301.UseVisualStyleBackColor = true;
+            // 
+            // radio_s1q1302
+            // 
+            this.radio_s1q1302.AutoSize = true;
+            this.radio_s1q1302.Location = new System.Drawing.Point(331, 256);
+            this.radio_s1q1302.Name = "radio_s1q1302";
+            this.radio_s1q1302.Size = new System.Drawing.Size(85, 17);
+            this.radio_s1q1302.TabIndex = 12;
+            this.radio_s1q1302.TabStop = true;
+            this.radio_s1q1302.Text = "radioButton3";
+            this.radio_s1q1302.UseVisualStyleBackColor = true;
+            this.radio_s1q1302.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // InfoActivity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 440);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radio_s1q1302);
+            this.Controls.Add(this.radio_s1q1301);
+            this.Controls.Add(this.combo_s1q8);
+            this.Controls.Add(this.combo_s1q6);
+            this.Controls.Add(this.date_s1q4);
+            this.Controls.Add(this.text_s1q2);
+            this.Controls.Add(this.text_s1q16);
+            this.Controls.Add(this.text__s1q15);
+            this.Controls.Add(this.text__s1q14);
+            this.Controls.Add(this.text_s1q12);
+            this.Controls.Add(this.text_s1q11);
+            this.Controls.Add(this.text_s1qno);
+            this.Controls.Add(this.label_s1q12);
+            this.Controls.Add(this.label_s1q16);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_s1q11);
+            this.Controls.Add(this.label_s1q15);
+            this.Controls.Add(this.label_s1q8);
+            this.Controls.Add(this.label_s1q14);
+            this.Controls.Add(this.label_s1q6);
+            this.Controls.Add(this.label_s1q13);
+            this.Controls.Add(this.label_s1q2);
+            this.Controls.Add(this.label_s1q4);
+            this.Controls.Add(this.label_s1qno);
+            this.Controls.Add(this.label1);
+            this.Name = "InfoActivity";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_s1qno;
+        private System.Windows.Forms.Label label_s1q4;
+        private System.Windows.Forms.Label label_s1q6;
+        private System.Windows.Forms.Label label_s1q8;
+        private System.Windows.Forms.Label label_s1q11;
+        private System.Windows.Forms.Label label_s1q12;
+        private System.Windows.Forms.Label label_s1q13;
+        private System.Windows.Forms.Label label_s1q14;
+        private System.Windows.Forms.Label label_s1q15;
+        private System.Windows.Forms.Label label_s1q16;
+        private System.Windows.Forms.TextBox text_s1qno;
+        private System.Windows.Forms.DateTimePicker date_s1q4;
+        private System.Windows.Forms.Label label_s1q2;
+        private System.Windows.Forms.TextBox text_s1q2;
+        private System.Windows.Forms.ComboBox combo_s1q6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox combo_s1q8;
+        private System.Windows.Forms.TextBox text_s1q11;
+        private System.Windows.Forms.TextBox text_s1q12;
+        private System.Windows.Forms.TextBox text__s1q14;
+        private System.Windows.Forms.TextBox text__s1q15;
+        private System.Windows.Forms.TextBox text_s1q16;
+        private System.Windows.Forms.RadioButton radio_s1q1301;
+        private System.Windows.Forms.RadioButton radio_s1q1302;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Win_Form_GB/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Form_GB
+{
+    public partial class InfoActivity : Form
+    {
+        public InfoActivity()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1qno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1q4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1q6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1q8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1q12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s1q11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radio_s1q1701_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radio_s1q1801_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radio_s1_consent01_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTime_s1q19et_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
